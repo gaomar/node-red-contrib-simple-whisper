2759f351063d1b186f46e5d3055e9608942f8a48
h-takauma <h-takauma@i-enter.co.jp>
2023-09-08 16:50:49 +0900

    初回コミット

---
+++ README.md
@@ -0,0 +1,46 @@
+[![platform](https://img.shields.io/badge/platform-Node--RED-red)](https://nodered.org)
+[![npm](https://img.shields.io/npm/v/node-red-contrib-simple-whisper.svg)](https://www.npmjs.com/package/node-red-contrib-simple-whisper)
+[![downloads](https://img.shields.io/npm/dt/node-red-contrib-simple-whisper.svg)](https://www.npmjs.com/package/node-red-contrib-simple-whisper)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/HaroldPetersInskipp/node-red-contrib-chatgpt/blob/main/LICENSE)
+
+## 概要
+ファイルから文字起こしを行います。
+
+文字起こしはWhisper APIを使って行っています。
+
+## インストール
+
+```
+npm i node-red-contrib-simple-whisper
+```
+
+or
+
+AdminタブからInstall
+
+## 使い方
+### 入力項目
+
+|項目|説明|
+|--|--|
+|OpenAI API Key|OpenAPIのAPIキーを設定します。|
+|payload|バイナリ形式のファイルを指定します。|
+|msg.file|バイナリ形式のファイルを直接指定します。（省略可能）|
+
+### 出力項目
+
+|項目|説明|
+|--|--|
+|payload|Whisperで文字起こしした結果が返ってきます。|
+
+
+## LINK
+
+* [NodeRED](https://flows.nodered.org/node/node-red-contrib-simple-whisper)
+* [Libraries.io](https://libraries.io/npm/node-red-contrib-simple-whisper)
+* [npm](https://www.npmjs.com/package/node-red-contrib-simple-whisper)
+
+## release
+
+* 2023/09/08: 初回リリース（v1.0.0）
+
---
+++ node.js
@@ -0,0 +1,49 @@
+module.exports = (RED) => {
+  const OpenAI = require("openai");
+  const main = function (config) {
+    RED.nodes.createNode(this, config);
+    this.Token = config.Token || "";
+    const node = this;
+
+    const openai = new OpenAI({
+      apiKey: node.Token,
+    });
+
+    node.on("input", async (msg) => {
+      node.status({ fill: "green", shape: "dot", text: "処理中..." });
+      if (typeof msg.file === "undefined") msg.file = Buffer.from('');
+      const buffer = (msg.file.length == 0 ) ? msg.payload : msg.file;
+      try {
+        const resp = await openai.audio.transcriptions.create({
+          file: await OpenAI.toFile(buffer, 'hoge.wav'),
+          model: 'whisper-1',
+        })
+        msg.payload = resp.text;
+        node.status({});
+      } catch (error) {
+        if (error.response) {
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `${error.response.status}, ${JSON.stringify(
+              error.response.data.error.message
+            )}`,
+          });
+          msg.payload = `${error.response.status}, ${JSON.stringify(
+            error.response.data.error.message
+          )}`;
+        } else {
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `${error.type}, ${error.message}`,
+          });
+          msg.payload = `${error.type}, ${error.message}`;
+        }
+      }
+      node.send(msg);
+    });
+  };
+
+  RED.nodes.registerType("simple-whisper", main);
+};
